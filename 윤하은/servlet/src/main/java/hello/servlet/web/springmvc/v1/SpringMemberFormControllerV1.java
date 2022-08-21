fa6f55f31e374d9f1f3d103e667bc85d181a6d56
hahaha329 <hahaha329@ewhain.net>
2022-08-22 04:06:46 +0900

    [05] 스프링 MVC - 실용적인 방식

--- 윤하은/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ 윤하은/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,4 +1,4 @@
-package hello.servlet.web.springmvc.old.v1;
+package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
